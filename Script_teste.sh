c28c2c82b98e66ac26e9e4085eb4b34abc244857
Atlas Unory <surfitasnierks@gmail.com>
2025-08-17 00:51:47 -0300

    Adding the test installation script

---
+++ Script_teste.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Updating the system
+sudo pacman -Syu
+
+# Main packages
+sudo pacman -S vlc git bat firefox eza zsh fastfetch ghostty keepassxc fzf yazi neovim vlc-plugins-all
+
+# installing yay
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+cd $HOME
+
+# Installing VScode
+yay -S visual-studio-code-bin
+
+# installing oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Installing syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+# Installing zsh-autosuggestions
+sudo pacman -S zsh-autosuggestions
+
+# Linking the Dotfiles
+
+ln -s ~/Documents/Dotfiles/fastfetch/ ~/.config/
+
+ln -s ~/Documents/Dotfiles/.zshrc/ ~/
+
+ln -s ~/Documents/Dotfiles/ghostty/ ~/.config/
