b74852d409a8801d2b36872ddebd3d391e6b6497
Atlas Unory <surfitasnierks@gmail.com>
2025-08-17 21:23:07 -0300

    Adding V2.0

--- Script_teste.sh
+++ Script_teste.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -e
+
+# Sudo password only in the begining
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Changing directory
 cd $HOME
 
@@ -10,17 +15,20 @@ sudo pacman -Syu --noconfirm
 sudo pacman -S vlc git bat firefox eza zsh fastfetch nvm ghostty keepassxc fzf yazi neovim vlc-plugins-all --noconfirm
 
 # installing yay
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm --needed
 cd $HOME
 
 # Installing VScode
-yay -S visual-studio-code-bin --noconfirm
+yay -S visual-studio-code-bin --noconfirm --needed
 
 # Installing Brave
-yay -S brave-bin --noconfirm
+yay -S brave-bin --noconfirm --needed
+
+# oh-my-zsh venv variables
+RUNZSH=no CHSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # installing oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --noconfirm
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Installing syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
@@ -34,14 +42,16 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # Verifing zshrc in home
 if [ -e ~/.zshrc ]; then
   rm -f .zshrc
-  ln -s ~/Documents/Dotfiles/.zshrc/ ~/
+  ln -sf ~/Documents/Dotfiles/.zshrc/ ~/.zshrc
 else
   echo "There is no .zshrc file in ~"
+fi
 
 # Installing lazyvim
-git clone https://github.com/LazyVim/starter ~/.config/nvim && rm -rf ~/.config/nvim/.git
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 
 # Linking the Dotfiles
-ln -s ~/Documents/Dotfiles/fastfetch/ ~/.config/
-ln -s ~/Documents/Dotfiles/ghostty/ ~/.config/
-ln -s ~/Documents/Dotfiles/fonts/ ~/.local/share/
+ln -sf ~/Documents/Dotfiles/fastfetch/ ~/.config/
+ln -sf ~/Documents/Dotfiles/ghostty/ ~/.config/
+ln -sf ~/Documents/Dotfiles/fonts/ ~/.local/share/
