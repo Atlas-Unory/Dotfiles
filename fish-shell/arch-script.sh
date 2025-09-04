64a663d009135fe5631ad6dd662c2a391e29c667
Atlas Unory <surfitasnierks@gmail.com>
2025-09-03 22:42:35 -0300

    Adding noconfirm in the arch install script

--- fish-shell/arch-script.sh
+++ fish-shell/arch-script.sh
@@ -4,19 +4,19 @@
 sudo pacman -Syu
 
 # Installing the basic packages
-pacman -S ghostty neovim fish fisher curl keepassxc fastfetch gimp speedtest-cli php-apache yazi yay wget btop unzip unrar virtualbox php ntfs-3g eza apache bat flatpak npm
+sudo pacman -S --noconfirm ghostty neovim fish fisher curl keepassxc fastfetch gimp speedtest-cli php-apache yazi yay wget btop unzip unrar virtualbox php ntfs-3g eza apache bat flatpak npm
 
 # Installing needed fonts
-pacman -S noto-fonts-extra noto-fonts-emoji noto-fonts-cjk
+sudo pacman -S --noconfirm noto-fonts-extra noto-fonts-emoji noto-fonts-cjk
 
 # Installing libreoffice
-pacman -S libreoffice-fresh libmythes mythes-en
+sudo pacman -S --noconfirm libreoffice-fresh libmythes mythes-en
 
 # Installing basic packages with Yay
-yay -S visual-studio-code-bin brave-bin
+yay -S --noconfirm visual-studio-code-bin brave-bin
 
 # Installing spotify
-yay -S spotify
+yay -S --noconfirm spotify
 
 # Installing Lazyvim
 git clone https://github.com/LazyVim/starter ~/.config/nvim && rm -rf ~/.config/nvim/.git
